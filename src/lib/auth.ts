b9eaeb0f6d694648ae6345d62ea0dce2c0d14622
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-06-08 22:49:27 +0530

    added google oauth

---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+import { db } from "@/db";
+import { account, session, user, verification } from "@/db/schema";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      user,
+      verification,
+      session,
+      account
+    }
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }
+  }
+});
