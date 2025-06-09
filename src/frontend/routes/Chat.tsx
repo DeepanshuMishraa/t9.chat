41a48084e58e65f825b8ab628d56da45358c9546
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-06-09 20:36:50 +0530

    chore

--- src/frontend/routes/Chat.tsx
+++ src/frontend/routes/Chat.tsx
@@ -1,11 +1,13 @@
 import ChatInput from "@/components/ChatInput";
+import { useSidebar } from "@/components/ui/sidebar";
 
 
 export default function Chat() {
+  const sidebar = useSidebar()
   return (
     <div className="flex flex-col h-full">
       <div className="relative">
-        <div className="fixed bottom-0 left-0 right-0 flex justify-center">
+        <div className={`fixed bottom-0 left-0 right-0 flex justify-center ${sidebar.open ? "md:left-64" : ""}`}>
           <ChatInput />
         </div>
       </div>
